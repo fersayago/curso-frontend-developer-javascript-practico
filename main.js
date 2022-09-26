c657bc40cb089af92a61138ecbdaaef389587b7a
fersayago <sayago.fernando@gmail.com>
2022-09-26 12:22:14 -0300

    Fusión del menú en mobile

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const menuEmail = document.getElementsByClassName('navbar-email')[0];
 const desktopMenu = document.getElementsByClassName('desktop-menu')[0];
+const mobileMenu = document.getElementsByClassName('mobile-menu')[0];
+const menuHamburgerIcon = document.getElementsByClassName('menu')[0];
 
 function toggleDesktopMenu(){
   desktopMenu.classList.toggle('inactive');
 }
+function toggleMobileMenu(){
+  mobileMenu.classList.toggle('inactive');
+}
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburgerIcon.addEventListener('click', toggleMobileMenu);
