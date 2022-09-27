6ec5e0a4802ade44f3c2424eb6429cdca88fb1a8
fersayago <sayago.fernando@gmail.com>
2022-09-27 03:03:03 -0300

    Carrito de compras

--- main.js
+++ main.js
@@ -1,14 +1,50 @@
-const menuEmail = document.getElementsByClassName('navbar-email')[0];
-const desktopMenu = document.getElementsByClassName('desktop-menu')[0];
-const mobileMenu = document.getElementsByClassName('mobile-menu')[0];
-const menuHamburgerIcon = document.getElementsByClassName('menu')[0];
+const menuEmail = document.getElementsByClassName("navbar-email")[0];
+const menuHamburgerIcon = document.getElementsByClassName("menu")[0];
+const menuCarritoIcon = document.getElementsByClassName(
+  "navbar-shopping-cart"
+)[0];
+const desktopMenu = document.getElementsByClassName("desktop-menu")[0];
+const mobileMenu = document.getElementsByClassName("mobile-menu")[0];
+const aside = document.getElementsByClassName("product-detail")[0];
 
-function toggleDesktopMenu(){
-  desktopMenu.classList.toggle('inactive');
+function toggleDesktopMenu() {
+  
+  const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    // sacar el aside
+    aside.classList.add("inactive");
+  }
+  desktopMenu.classList.toggle("inactive");
 }
-function toggleMobileMenu(){
-  mobileMenu.classList.toggle('inactive');
+function toggleMobileMenu() {
+  const isAsideClosed = aside.classList.contains('inactive');
+
+  if (!isAsideClosed) {
+    // sacar el aside
+    aside.classList.add("inactive");
+  }
+
+  mobileMenu.classList.toggle("inactive");
 }
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
-menuHamburgerIcon.addEventListener('click', toggleMobileMenu);
+function toggleCart() {
+  const isMobileMenuClosed = mobileMenu.classList.contains("inactive");
+  const isMenuEmailClosed = menuEmail.classList.contains("inactive");
+
+  if (!isMobileMenuClosed) {
+    // sacar el mobile menu
+    mobileMenu.classList.add("inactive");
+  }
+
+  if (!isMenuEmailClosed) {
+    // sacar el mobile menu
+    desktopMenu.classList.add("inactive");
+  }
+
+  aside.classList.toggle("inactive");
+}
+
+menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamburgerIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCart);
